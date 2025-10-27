925003c7b8ec79ae04ba4151a6132353ad9ad307
DefffinitiveOx72 <166840943+DefffinitiveOx72@users.noreply.github.com>
2025-10-27 20:44:53 +0300

    9

--- js/main.js
+++
@@ -1,8 +0,0 @@
-import { getPhotosDataArray } from './data.js';
-import { renderThumbnails } from './thumbnails.js';
-import './fullsize.js';
-import './upload-form.js';
-import './filter.js';
-
-getPhotosDataArray();
-renderThumbnails();
